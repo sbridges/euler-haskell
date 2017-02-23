439a5c8e44226e608afab327b2c199d8d17140cb
Sean Bridges <sean.bridges@globalrelay.net>
2017-02-22 21:45:00 -0800

    euler1

--- euler/src/Main.hs
+++ euler/src/Main.hs
@@ -1,5 +1,15 @@
 module Main where
 
+import System.Environment
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  arg <- fmap head getArgs 
+  let answer = show (
+                      case arg of "1" -> euler1
+                    ) 
+  putStrLn answer
+  return ()
+
+euler1 :: Int
+euler1 = sum ( filter (\x -> mod x 3 == 0 || mod x 5 == 0) [1..999] )
