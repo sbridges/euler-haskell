51b73a9325f3aa960d2864056947b2890ecd9761
Sean Bridges <sean.bridges@globalrelay.net>
2017-02-23 21:05:39 -0800

    euler2

--- euler/src/Main.hs
+++ euler/src/Main.hs
@@ -7,9 +7,21 @@ main = do
   arg <- fmap head getArgs 
   let answer = show (
                       case arg of "1" -> euler1
+                                  "2" -> euler2
                     ) 
   putStrLn answer
   return ()
 
 euler1 :: Int
 euler1 = sum ( filter (\x -> mod x 3 == 0 || mod x 5 == 0) [1..999] )
+
+nextFib :: (Int, Int) -> (Int, Int)
+nextFib (a,b) = (a + b, a)
+
+
+fib :: [Int]
+fib = fmap fst (iterate nextFib (1,0))
+
+euler2 :: Int
+euler2 = sum ( filter (even) (takeWhile (\x ->  x < 4000000) fib))
+
